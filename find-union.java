1ce2a22d4537dd5d248805732da31e8627896c20
marsPure <bpourtaherian@yahoo.com>
2017-07-10 10:42:15 -0700

    Create find-union.java

---
+++ find-union.java
@@ -0,0 +1,8 @@
+// union: connect 2 nodes
+// connected : determine if 2 nodes are connected or not
+
+//assumtions : 
+//  reflexive : p is connected to p
+//  symmetric : if p is connected to q, q is connected to p.
+//  transitive: if p is connected to q and q is connected to r, then p is connected to r.
+
