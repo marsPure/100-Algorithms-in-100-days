cf2d88ca9cb8a512f0d02f4110b6d4e3e7608bd8
marsPure <bpourtaherian@yahoo.com>
2017-07-13 21:17:50 -0700

    Update 4.quickSort.java

--- 4.quickSort.java
+++ 4.quickSort.java
@@ -1,5 +1,44 @@
 //quick sort 
 // have to shuffle first
-// 
+// chose a partitioning position( here a[lo] ) 
+// reduce right side pointer until find a value smaller than partiioning position
+// increment left side pointer until find a value bigger than partitioning position
+// swap left and right pointers value
+// when j>i swap a[j] with a[lo]
 
-private static int partition
+public class QuickSort
+{
+  private static int partition(Comparable[] a, int lo, int hi)
+  {
+     int i=lo, j=hi+1;
+    //partitioning element a[lo]
+    while(true)
+    {
+      while(less(a[++i],a[lo]))
+        if(i==hi) break;
+
+      while(less(a[lo],a[--j]))
+        if(j==lo) break;
+
+      if(i>=j) break;
+      exch(a, i, j);
+
+    }
+    exch(a,lo,j);
+    return j;
+  }
+  
+  public static void sort(Comparable[] a)
+  {
+    StdRandom.shuffle(a);
+    sort(a,0,a.length-1);
+  }
+  
+  private static void sort(Comparable[] a, int lo, int hi)
+  {
+    if(hi <= lo) return;
+    int j = partition(a, lo, hi);
+    sort(a, lo, j-1);
+    sort(a, j+1, hi);
+  }
+}
