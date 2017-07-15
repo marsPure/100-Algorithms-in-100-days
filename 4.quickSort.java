7103bf88106866714702203db531c39cccdb47c9
marsPure <bpourtaherian@yahoo.com>
2017-07-14 18:23:35 -0700

    Update 4.quickSort.java

--- 4.quickSort.java
+++ 4.quickSort.java
@@ -42,3 +42,18 @@ public class QuickSort
     sort(a, j+1, hi);
   }
 }
+
+// worst case is quadratic 1/2 N^2 that is why we have to shuffle it first.
+// average case is 1.39 NlogN
+// 38% more compares than mergesort, but because it doesn't copy files in and out of aux array it is faster.
+
+//if there are lots of duplicate items it may become quadratic 
+
+//quicksort it not stable
+
+//ways to make it faster
+//  use insertion sort for small subarrays (=10) or leave the smaller subarrays unsorted and run one insertion sort at the end
+// sample 3 items and chose the medium as partitioning element
+
+
+
