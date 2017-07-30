6c67bd69fe92d836efb3c8f243d6341aa7db7f1a
marsPure <bpourtaherian@yahoo.com>
2017-07-30 13:17:17 -0700

    Update 12-LinearProbingHashTable.java

--- 12-LinearProbingHashTable.java
+++ 12-LinearProbingHashTable.java
@@ -1,1 +1,29 @@
-public clas
+public class LinearProbingHashTable
+{
+    private int M= 30001;
+    private Value[] vals = (Value[]) new Object[M];
+    private Key[] keys = (Keys[]) new Object[M];
+    
+    private int hash(Key key){}
+    
+    public Value get(Key key){
+        int h = hash(key);
+        while(keys[h]!=null){
+            if(keys[h].equals(key))
+                return vals[h];
+            else h=(h+1) % M;
+        }
+        return null;
+    }
+    
+    public void put(Key key, Value val){
+        int h = hash(key);
+        while(keys[h] != null){
+            if(keys[h].equals(key))
+                break;
+            h=(h+1) % M;
+        }
+        keys[h]=key;
+        vals[h]=val;
+    }
+}
